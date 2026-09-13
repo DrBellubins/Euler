23a111a7824982d49e400b2e8ee11bcbfaaa86f0
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-09-13 14:34:15 -0400

    Start 3d scene (add freecam)

--- Euler/Scenes/TestScene.cs
+++ Euler/Scenes/TestScene.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using Euler.GameEngine;
+using Euler.Gameplay;
 using Euler.Utils;
 using Raylib_cs;
 
@@ -8,44 +9,28 @@ namespace Euler.Scenes;
 public class TestScene : Scene
 {
     public TestScene(string name) : base(name) { }
-
-    private Camera3D cameraTest;
     
+    private Freecam freecam = new();
+
     public override void Start()
     {
-        cameraTest = new Camera3D();
-        cameraTest.FovY = 90f;
-        cameraTest.Position = new Vector3(0, 0, -10);
-        cameraTest.Up = Vector3.UnitY;
-        cameraTest.Projection = CameraProjection.Perspective;
+        freecam.Start();
     }
 
     public override void Update()
     {
-        if (Input.MoveForward())
-            cameraTest.Position += Vector3.UnitZ * Time.DeltaTimeF * 10;
-        
-        if (Input.MoveBackward())
-            cameraTest.Position -= Vector3.UnitZ * Time.DeltaTimeF * 10;
-        
-        if (Input.MoveLeft())
-            cameraTest.Position += Vector3.UnitX * Time.DeltaTimeF * 10;
-        
-        if (Input.MoveRight())
-            cameraTest.Position -= Vector3.UnitX * Time.DeltaTimeF * 10;
+        freecam.Update();
     }
 
     public override void Draw()
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.Black);
-        
-        Raylib.BeginMode3D(cameraTest);
-        
+
+        Raylib.BeginMode3D(freecam.Camera);
         Raylib.DrawCubeV(Vector3.Zero, Vector3.One, Color.Orange);
-        
         Raylib.EndMode3D();
-        
+
         Raylib.EndDrawing();
     }
 }
